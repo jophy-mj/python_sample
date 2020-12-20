5ed46eacdb8377c5ae62bd8d6968db944d98f949
jophy-mj <jophymj63@gmail.com>
2020-12-20 19:27:25 +0530

    committed python programs

---
+++ c1programs/p5.py
@@ -0,0 +1,7 @@
+a=int(input("enter 1st no:"))
+b=int(input("enter 2nd no:"))
+q=a/b
+r=a%b
+print("quotient:",q)
+print("remainder:",r)
+      
---
+++ c1programs/p7.py
@@ -0,0 +1,6 @@
+a=input("enter the no:")
+c=int(a)%2
+if(c==0):
+    print(a,"is an even no:")
+else:
+    print(a,"is an odd no:")
